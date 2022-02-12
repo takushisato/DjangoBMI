84759784c536b5b956de76a99723cb77f002fab9
佐藤拓史 <>
2022-02-12 15:04:01 +0900

    新規投稿機能の途中、VScodeの設定を変更したので、ちょっとテストでコミット

--- mysite/settings.py
+++ mysite/settings.py
@@ -10,8 +10,6 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
-# kanrisyanobu
-
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
--- website/forms.py
+++ website/forms.py
@@ -1,9 +1,6 @@
 from django import forms
 
 class UserdateForm(forms.Form):
-    def __init__(self, user, *args, **kwargs):
-        self.login_user = user
-        super().__init__(*args, **kwargs)
 
     height = forms.FloatField(label = '身長')
     weight = forms.FloatField(label = '体重')
